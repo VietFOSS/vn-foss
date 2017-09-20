128ecaa95deb17b089f418f726a30a8397092853
Le Quoc Viet <vietlq85@gmail.com>
2017-09-20 07:05:44 +0100

    Update query_github.py

--- query_github.py
+++ query_github.py
@@ -1,7 +1,9 @@
 import os, requests
 
 GH_PERSONAL_TOKEN = os.environ['GH_PERSONAL_TOKEN']
-GH_GRAPHQL_EP = 'https://api.github.com/graphql'
+GH_GRAPHQL_EP = '''https://api.github.com/graphql'''
+GH_AUTH_VALUE = "bearer %s" % GH_PERSONAL_TOKEN
+GH_HEADERS = {"Authorization": GH_AUTH_VALUE}
 
 QUERY_USER_BASIC_INFO = '''
 {
@@ -15,4 +17,4 @@ QUERY_USER_BASIC_INFO = '''
 }
 '''
 
-requests.post(GH_GRAPHQL_EP, data=QUERY_USER_BASIC_INFO)
+requests.post(GH_GRAPHQL_EP, data=QUERY_USER_BASIC_INFO, headers=GH_HEADERS)
